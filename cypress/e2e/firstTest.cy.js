e0e025f4a732188ed87e8c354948c6df1feba489
IrinaNag <nagornovairina@gmail.com>
2022-07-25 23:18:03 +0300

    lector about finding elements by Get, Find, Eq

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress"/>
+
+// describe('Mobile phone replenishment', () => {
+
+//   it('Check min sum', () => {
+//     //
+//   })
+
+// })
+
+it('Using Get with Find and Eq', () => {
+
+  cy.visit('/deposit/open');
+  //cy.get('tbody').find('tr').eq(0).find('td').eq(7);
+  cy.get('tbody').find('td').find('button').eq(0);
+
+})
+
+it.only('Using Get with Find and Eq', () => {
+
+  cy.viewport(1920, 1080);
+  cy.visit('https://docs.cypress.io/api/commands/eq');
+  cy.get('main').find('nav').find('li').find('a').eq(0);
+
+})
