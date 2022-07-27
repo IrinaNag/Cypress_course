72027ec312660b1ce8dfe1d2c268e58d9b33c938
IrinaNag <nagornovairina@gmail.com>
2022-07-27 15:55:22 +0300

    containsLesson

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,1 +1,16 @@
 /// <reference types="Cypress"/>
+
+it('', () => {
+    cy.visit('/mobile?lang=en');
+    cy.contains('Sign in');
+})
+
+it('', () => {
+    cy.visit('/mobile?lang=en');
+    cy.contains('div', 'Sign in');
+})
+
+it.only('', () => {
+    cy.visit('/mobile?lang=en');
+    cy.contains('div', 'SIGN IN', {matchCase: false});
+})
