9c239b2d20fb1f6fbb4de83f4be1d56127ee823c
IrinaNag <nagornovairina@gmail.com>
2022-07-28 16:48:32 +0300

    sssresrsr

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,1 +1,16 @@
 /// <reference types="Cypress"/>
+
+it('check is button attr correct', () => {
+    cy.visit('?lang=en');
+    cy.contains('Show cards')
+        .should('have.attr', 'type')
+        .and('match', /button/);
+})
+
+it.only('check is URL correct', () => {
+    cy.visit('?lang=en');
+    cy.url()
+        .should('eq', 'https://next.privat24.ua/?lang=en');
+})
+
+
